1faf9df646806ad00435ace6e8f9bd5d3d8ecb52
kaymas <samyakahuja24@hotmail.com>
2018-05-01 21:38:25 +0530

    added bfs function and other UI

--- s1/02_bfs/graph.js
+++ s1/02_bfs/graph.js
@@ -1,6 +1,8 @@
 function Graph(){
     this.nodes = []
     this.graph = {}
+    this.start = null
+    this.end = null
 }
 
 Graph.prototype.addNode = function(n){
@@ -11,4 +13,21 @@ Graph.prototype.addNode = function(n){
 
 Graph.prototype.getNode = function(actor){
     return this.graph[actor]
+}
+
+Graph.prototype.setStart = function(actor){
+    this.start = this.graph[actor]
+    return this.start
+}
+
+Graph.prototype.setEnd = function(actor){
+    this.end = this.graph[actor]
+    return this.end
+}
+
+Graph.prototype.reset = function(){
+    for(var i = 0; i < this.nodes.length; i++){
+        this.nodes[i].searched = false
+        this.nodes[i].parent = null
+    }
 }
--- s1/02_bfs/sketch.js
+++ s1/02_bfs/sketch.js
@@ -1,5 +1,6 @@
 let data
 let graph
+let dropdown
 
 //these are p5 methods
 function preload(){
@@ -8,7 +9,8 @@ function preload(){
 
 function setup(){
     noCanvas()
-
+    dropdown = createSelect()
+    dropdown.changed(bfs)
     //graph contains both actors and movies as nodes from bacon.json
     graph = new Graph()
 
@@ -26,10 +28,63 @@ function setup(){
             let actorNode = graph.getNode(actor)
             if(actorNode == undefined){
                 actorNode = new Node(actor)
+                graph.addNode(actorNode)   
+                dropdown.option(actor)  
             }
-            graph.addNode(actorNode)     
             movieNode.addEdge(actorNode)       
         }
     }
+
     console.log(graph)
+
+}
+
+function bfs(){
+    graph.reset()
+
+    let start = graph.setStart(dropdown.value())
+    let end = graph.setEnd("Kevin Bacon")
+
+    let queue = []
+    
+    start.searched = true
+    queue.push(start)
+
+    while(queue.length > 0){
+        let current = queue.shift() //removes first element from queue and returns it.
+        if(current == end){
+            console.log("found " + current.value);
+            break
+        }
+        let edges = current.edges
+        for(var i = 0; i < edges.length; i++){
+            let neighbour = edges[i]
+            if(!neighbour.searched){
+                neighbour.searched = true
+                neighbour.parent = current
+                queue.push(neighbour)
+            }
+        }
+    }
+
+    //Creating the path from end to start
+    let path = []
+    
+    path.push(end)
+    let next = end.parent
+    while(next != null){
+        path.push(next)
+        next = next.parent
+    }
+    console.log(path);
+    
+    //Displaying the path from start to end
+    let txt = ''
+    for(var i = path.length - 1; i > 0; i--){
+        let node = path[i]
+        txt += node.value + ' --> '
+    }
+    txt += end.value
+    createP(txt)
+
 }
