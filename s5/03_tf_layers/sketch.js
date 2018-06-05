1c2445bf62dc802e04ee0a370b8af2153090ffc1
kaymas <samyakahuja24@hotmail.com>
2018-06-05 21:03:09 +0530

    data fit and predict on sample

--- s5/03_tf_layers/sketch.js
+++ s5/03_tf_layers/sketch.js
@@ -7,16 +7,47 @@ const hidden = tf.layers.dense({
     activation : 'sigmoid'
 })
 const output = tf.layers.dense({
-    units : 3,
+    units : 1,
     activation: 'sigmoid'
 })
 
 model.add(hidden)
 model.add(output)
 
-const learningRate = 0.01
+const learningRate = 0.5
 
 model.compile({
     optimizer : tf.train.sgd(learningRate),
     loss : 'meanSquaredError'
+})
+
+//shitty data
+const inputs = tf.tensor([
+    [0,0],
+    [0.5,0.5],
+    [1,1]
+])
+const outputs = tf.tensor([
+    [1],
+    [0.5],
+    [0]
+])
+const predictData = tf.tensor([
+    [0.1,0.04],
+    [0.29,0.3],
+    [0.51,0.43],
+    [0.74,0.63],
+    [0.98,0.89]
+])
+model.predict(inputs).print()
+async function train() {
+    //500 epochs
+    for(let i = 0; i < 500; i++){
+        let response = await model.fit(inputs, outputs, {shuffle: true})
+        console.log(response.history.loss[0])
+    }
+}
+train().then(() => {
+    console.log('training completed');
+    model.predict(predictData).print()
 })
